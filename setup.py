0f330574a1c42629ef64ed288b59f1aba59ceb5a
golddoushi <863075773@qq.com>
2020-06-01 02:17:55 +0800

    modify setup.py to support compiling of c

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 # -*- coding: UTF-8 -*-
+from distutils.core import Extension, setup
 import setuptools
 
 with open("README.md", "r", encoding='UTF-8') as fh:
     long_description = fh.read()
- 
-setuptools.setup(
+
+isingLib=setuptools.Extension('isinglib.so',sources=['./isingLib.c'],language='c',extra_compile_args=['-std=c99','-fPIC'])
+#print(isingLib)
+#xyLib=Extension('xylib.so',sources=['./xyLib.c'])
+#heisenbergLib=Extension('heisenberglib.so',sources=['./heisenbergLib.c']) 
+setup(
     name="mcsolver",
     version="1.0.2",
     author="Liang Liu",
@@ -15,6 +20,7 @@ setuptools.setup(
     url="https://github.com/golddoushi/mcsolver",
     packages=setuptools.find_packages(),
     include_package_data=True,
+    ext_modules=[isingLib],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: GNU License v3",
